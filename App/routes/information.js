fcad0e2b71bb23f62e1bc01ad86ab22d95a20ea1
lll-jy <E0376997@u.nus.edu>
2020-12-20 10:39:27 +0800

    Logged in with database data

--- App/routes/information.js
+++ App/routes/information.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config({path: __dirname + '/../.env'});
+
+const { Pool } = require('pg')
+const pool = new Pool({connectionString:process.env.DATABASE_URL})
+
+var username;
+var nric;
+var age;
+var career;
+var diabetes;
+var cancer;
+var heartDisease;
+var pregnancy;
+
+var select_query = 'SELECT * FROM customers WHERE nric=$1';
+
+var renderPage = (res) => {
+  res.render('information', {
+    title: 'Information',
+    username: username,
+    nric: nric,
+    age: age,
+    career: career,
+    diabetes: diabetes,
+    cancer: cancer,
+    heartDisease: heartDisease,
+    pregnancy: pregnancy
+  })
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('information', { title: 'Information' });
+router.get('/:nric', function(req, res, next) {
+  nric = req.params.nric;
+  pool.query(select_query, [nric], (err, data) => {
+    var customer = data.rows[0];
+    username = customer.name;
+    age = customer.age;
+    career = customer.career;
+    diabetes = customer.diabetes;
+    cancer = customer.cancer;
+    heartDisease = customer.heartDisease;
+    pregnancy = customer.pregnancy;
+    renderPage(res);
+  })
 });
 
 router.post('/', function(req, res, next) {
